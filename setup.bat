bbd9e5bc4f668a73942c88e25ba79b19c793e96c
Lobanz <lobanz@protonmail.com>
2025-02-22 00:52:10 -0600

    Added initial files

---
+++ setup.bat
@@ -0,0 +1,35 @@
+@echo off
+
+set "search1=MACRO_DIR/files/"
+set "replace1=%~dp0files\"
+set "search2=MACRO_DIR/"
+set "replace2=%~dp0"
+
+
+if exist "NARPNet_NCS.mdf.orig" (
+  echo ####################################
+  echo You can't run setup.bat twice.
+  echo Delete NARPNet_NCS.mdf, then rename NARPNet_NCS.mdf.orig to NARPNet_NCS.mdf.
+  echo This will let you run it again.
+  echo ####################################
+) else (
+  echo Fixing file paths in NARPNet_NCS.mdf to match your macro location:
+  echo   %replace2%
+
+  rename NARPNet_NCS.mdf NARPNet_NCS.mdf.orig
+  rename NARPNet_OP.mdf NARPNet_OP.mdf.orig
+
+  powershell -Command "(gc NARPNet_NCS.mdf.orig) -replace '%search1%', '%replace1%' | Out-File -encoding ASCII NARPNet_NCS.mdf.1"
+  powershell -Command "(gc NARPNet_OP.mdf.orig) -replace '%search1%', '%replace1%' | Out-File -encoding ASCII NARPNet_OP.mdf.1"
+
+  powershell -Command "(gc NARPNet_NCS.mdf.1) -replace '%search2%', '%replace2%' | Out-File -encoding ASCII NARPNet_NCS.mdf."
+  powershell -Command "(gc NARPNet_OP.mdf.1) -replace '%search2%', '%replace2%' | Out-File -encoding ASCII NARPNet_OP.mdf."
+
+  del NARPNet_NCS.mdf.1 NARPNet_OP.mdf.1
+
+  echo.
+  echo Done.
+  echo.
+)
+
+pause
---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/usr//bin/bash
+
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
+search1=MACRO_DIR/files/
+replace1=$SCRIPT_DIR/files/
+search2=MACRO_DIR/
+replace2=$SCRIPT_DIR/
+
+echo $search1
+echo $replace1
+echo $search2
+echo $replace2
+
+
+if [ -f NARPNet_NCS.mdf.orig ]
+then
+  echo "####################################"
+  echo "You can't run setup.bat twice."
+  echo "Delete NARPNet_NCS.mdf, then rename NARPNet_NCS.mdf.orig to NARPNet_NCS.mdf."
+  echo "This will let you run it again."
+  echo "####################################"
+else
+  echo "Fixing file paths in NARPNet macro files to match your macro location:"
+  echo "  $replace2"
+
+  cp NARPNet_NCS.mdf NARPNet_NCS.mdf.orig
+  cp NARPNet_OP.mdf NARPNet_OP.mdf.orig
+
+  sed -i "s/\r$//" NARPNet_NCS.mdf
+  sed -i "s;$search1;$replace1;g" NARPNet_NCS.mdf
+  sed -i "s;$search2;$replace2;g" NARPNet_NCS.mdf
+  
+  sed -i "s/\r$//" NARPNet_OP.mdf
+  sed -i "s;$search1;$replace1;g" NARPNet_OP.mdf
+  sed -i "s;$search2;$replace2;g" NARPNet_OP.mdf
+  
+  echo
+  echo "Done."
+  echo
+fi
+
+read -p "Press enter to continue"
+
