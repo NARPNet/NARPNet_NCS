c00c64b2d4d4cc9d825c05780073868276237760
Lobanz <lobanz@protonmail.com>
2025-06-29 18:15:41 -0500

    1.0-alpha changes

--- src/NARPNet_NCS/setup.bat
+++ src/NARPNet_NCS/setup.bat
@@ -17,14 +17,20 @@ if exist "NARPNet_NCS.mdf.orig" (
 
   rename NARPNet_NCS.mdf NARPNet_NCS.mdf.orig
   rename NARPNet_OP.mdf NARPNet_OP.mdf.orig
+  rename NARPNet_NET.mdf NARPNet_NET.mdf.orig
+  copy PERSONAL.mdf PERSONAL.orig
 
   powershell -Command "(gc NARPNet_NCS.mdf.orig) -replace '%search1%', '%replace1%' | Out-File -encoding ASCII NARPNet_NCS.mdf.1"
   powershell -Command "(gc NARPNet_OP.mdf.orig) -replace '%search1%', '%replace1%' | Out-File -encoding ASCII NARPNet_OP.mdf.1"
+  powershell -Command "(gc NARPNet_NET.mdf.orig) -replace '%search1%', '%replace1%' | Out-File -encoding ASCII NARPNet_NET.mdf.1"
+  powershell -Command "(gc PERSONAL.orig) -replace '%search1%', '%replace1%' | Out-File -encoding ASCII PERSONAL.1"
 
   powershell -Command "(gc NARPNet_NCS.mdf.1) -replace '%search2%', '%replace2%' | Out-File -encoding ASCII NARPNet_NCS.mdf."
   powershell -Command "(gc NARPNet_OP.mdf.1) -replace '%search2%', '%replace2%' | Out-File -encoding ASCII NARPNet_OP.mdf."
+  powershell -Command "(gc NARPNet_NET.mdf.1) -replace '%search2%', '%replace2%' | Out-File -encoding ASCII NARPNet_NET.mdf"
+  powershell -Command "(gc PERSONAL.1) -replace '%search2%', '%replace2%' | Out-File -encoding ASCII PERSONAL.mdf"
 
-  del NARPNet_NCS.mdf.1 NARPNet_OP.mdf.1
+  del NARPNet_NCS.mdf.1 NARPNet_OP.mdf.1 NARPNet_NET.mdf.1 PERSONAL.1
 
   echo.
   echo Done.
--- src/NARPNet_NCS/setup.sh
+++ src/NARPNet_NCS/setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr//bin/bash
 
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
@@ -6,6 +6,11 @@ search1=MACRO_DIR/files/
 replace1=$SCRIPT_DIR/files/
 search2=MACRO_DIR/
 replace2=$SCRIPT_DIR/
+
+echo $search1
+echo $replace1
+echo $search2
+echo $replace2
 
 if [ -f NARPNet_NCS.mdf.orig ]
 then
@@ -20,6 +25,9 @@ else
 
   cp NARPNet_NCS.mdf NARPNet_NCS.mdf.orig
   cp NARPNet_OP.mdf NARPNet_OP.mdf.orig
+  cp NARPNet_NET.mdf NARPNet_NET.mdf.orig
+  cp PERSONAL.mdf PERSONAL.orig
+
 
   sed -i "s/\r$//" NARPNet_NCS.mdf
   sed -i "s;$search1;$replace1;g" NARPNet_NCS.mdf
@@ -28,6 +36,14 @@ else
   sed -i "s/\r$//" NARPNet_OP.mdf
   sed -i "s;$search1;$replace1;g" NARPNet_OP.mdf
   sed -i "s;$search2;$replace2;g" NARPNet_OP.mdf
+  
+  sed -i "s/\r$//" NARPNet_NET.mdf
+  sed -i "s;$search1;$replace1;g" NARPNet_NET.mdf
+  sed -i "s;$search2;$replace2;g" NARPNet_NET.mdf
+  
+  sed -i "s/\r$//" PERSONAL.mdf
+  sed -i "s;$search1;$replace1;g" PERSONAL.mdf
+  sed -i "s;$search2;$replace2;g" PERSONAL.mdf
   
   echo
   echo "Done."
